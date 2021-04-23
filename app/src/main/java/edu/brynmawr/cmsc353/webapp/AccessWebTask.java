c5e4823a98969e973b1c0daa5b0c418d8823d616
wangjenny56 <xjennyx3@gmail.com>
2021-04-23 10:12:43 -0400

    initial commit

--- app/src/main/java/edu/brynmawr/cmsc353/webapp/AccessWebTask.java
+++ app/src/main/java/edu/brynmawr/cmsc353/webapp/AccessWebTask.java
@@ -2,19 +2,26 @@ package edu.brynmawr.cmsc353.webapp;
 
 import android.os.AsyncTask;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
-public class AccessWebTask extends AsyncTask<URL, String, String> {
+public class AccessWebTask extends AsyncTask<URL, String, List<Map<String, String>>> {
 
     @Override
-    protected String doInBackground(URL... urls) {
+    protected List<Map<String, String>> doInBackground(URL... urls) {
 
         try {
+            List<Map<String, String>> listings = new ArrayList<>();
             URL url = urls[0];
 
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -24,19 +31,34 @@ public class AccessWebTask extends AsyncTask<URL, String, String> {
             Scanner in = new Scanner(url.openStream());
             String response = in.nextLine();
 
-            JSONObject jo = new JSONObject(response);
-            String status = jo.getString("status");
-            return status;
+            JSONArray arrayResult = new JSONArray(response);
+            JSONObject listing = arrayResult.getJSONObject(0);
+
+            for(int i = 0; i < arrayResult.length(); i++){
+                HashMap<String,String> item = new HashMap<>();
+                item.put("food_description", listing.getString("food_description"));
+                item.put("food_type", listing.getString("food_type"));
+                item.put("quantity", listing.getString("quantity"));
+                item.put("perishability", listing.getString("perishability"));
+                item.put("pick_up_time", listing.getString("pick_up_time"));
+                item.put("availability_status", listing.getString("availability_status"));
+                item.put("picked_up_by", listing.getString("picked_up_by"));
+
+                listings.add(item);
+            }
+
+            //return listing.getString("food_description");
+            return listings;
 
         }
         catch (Exception e) {
-            return e.toString();
+            return Collections.emptyList();
         }
 
     }
 
     @Override
-    protected void onPostExecute(String s) {
+    protected void onPostExecute(List<Map<String, String>> list) {
         // this method would be called in the UI thread after doInBackground finishes
         // it can access the Views and update them asynchronously
     }
--- app/src/main/java/edu/brynmawr/cmsc353/webapp/MainActivity.java
+++ app/src/main/java/edu/brynmawr/cmsc353/webapp/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.TextView;
 
 import java.net.URL;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,13 +27,17 @@ public class MainActivity extends AppCompatActivity {
             // assumes that there is a server running on the AVD's host on port 3000
             // and that it has a /test endpoint that returns a JSON object with
             // a field called "status"
-            URL url = new URL("http://10.0.2.2:3000/test");
-
+            //URL url = new URL("http://10.0.2.2:3000/test");
+            URL url = new URL("http://10.0.2.2:3000/viewListingsForSocialService?zipcode=19010");
             AccessWebTask task = new AccessWebTask();
             task.execute(url);
-            String status = task.get();
+            List<Map<String, String>> listings = task.get();
+            String item = "";
 
-            tv.setText(status);
+            for(Map<String, String> listing: listings){
+                item = "food description: " + listing.get("food_description");
+            }
+            tv.setText(item);
 
         }
         catch (Exception e) {
